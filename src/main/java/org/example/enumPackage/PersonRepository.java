f28dc2279408caf1f3c0e0a2b8bcc73d1acbcd20
KroegerLeif <Leif.kr@outlook.com>
2025-09-05 11:15:51 +0200

    counts how many people per gender

--- src/main/java/org/example/enumPackage/PersonRepository.java
+++ src/main/java/org/example/enumPackage/PersonRepository.java
@@ -18,6 +18,29 @@ public class PersonRepository {
         return persons;
     }
 
+    public int[] countPersonProGender(){
+        int[] result = new int[3];
+        //Counts how many people are male, female and other
+        for (Person person : persons) {
+            switch(person.gender()){
+                case MALE:
+                    result[0]++;
+                    break;
+                case FEMALE:
+                    result[1]++;
+                    break;
+                case OTHER:
+                    result[2]++;
+                    break;
+                default:
+                    //Should never happen
+                    System.err.println("Error: Invalid gender");
+                    return null;
+            }
+        }
+        return result;
+    }
+
     //Boiler Plate Code
     @Override
     public boolean equals(Object o) {
