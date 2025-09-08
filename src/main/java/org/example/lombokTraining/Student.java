1b4085a5852dd4552b21b421bc72f5f3c31365d9
KroegerLeif <Leif.kr@outlook.com>
2025-09-08 11:45:52 +0200

    Cration of Student with lombok

---
+++ src/main/java/org/example/lombokTraining/Student.java
@@ -0,0 +1,11 @@
+package org.example.lombokTraining;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private int id;
+    private String name;
+    private String address;
+    private int grade;
+}
